cbb9d85dc59660f5d6830adf1d8bbf5851932a0f
cemb4 <cemb4@yandex.ru>
2023-04-12 18:25:35 +0300

    cemb4

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,61 @@
+package ru.itmentor.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(int id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(null);
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(int id, User updateUser) {
+        updateUser.setId(id);
+        userRepository.save(updateUser);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+}
