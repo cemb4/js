c98e60b63287fd25c815c9220d4707c7a584630a
cemb4 <cemb4@yandex.ru>
2023-04-21 18:06:07 +0300

    cemb4

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/DAO/UserDAO.java
@@ -0,0 +1,65 @@
+package ru.itmentor.spring.boot_security.demo.DAO;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.transaction.annotation.Transactional;
+import ru.itmentor.spring.boot_security.demo.model.Role;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+@Repository
+public class UserDAO {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserDAO(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public boolean saveUser(User user) {
+        Optional<User> userFromDB = userRepository.findByEmail(user.getName());
+
+        if (!userFromDB.isEmpty()) {
+            return false;
+        }
+
+        user.setRoles(Collections.singleton(new Role(2, "ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+
+    @Transactional
+    public void deleteUser(int userId) {
+        userRepository.deleteById(userId);
+    }
+
+
+    public Optional<User> findUserById(int userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Transactional
+    public void updateUserByIdAndUser(User user, int userId) {
+        user.setId(userId);
+        userRepository.save(user);
+    }
+
+    public List<String> getRoleInStringById(int id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.isEmpty() ? null : user.get().getRoles().stream().map(role -> role.getName()).collect(Collectors.toList());
+
+    }
+}
--- src/main/java/ru/itmentor/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/service/UserService.java
@@ -1,66 +1,49 @@
 package ru.itmentor.spring.boot_security.demo.service;
 
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import ru.itmentor.spring.boot_security.demo.DAO.UserDAO;
 import ru.itmentor.spring.boot_security.demo.model.User;
-import ru.itmentor.spring.boot_security.demo.model.Role;
 import ru.itmentor.spring.boot_security.demo.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 
 @Service
-public class UserService{
-
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+public class UserService extends UserDAO {
 
     public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository = userRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-
+        super(userRepository, bCryptPasswordEncoder);
     }
 
+    @Override
     public List<User> findAllUser() {
-        return userRepository.findAll();
+        return super.findAllUser();
     }
-    @Transactional
+
+    @Override
     public boolean saveUser(User user) {
-        Optional<User> userFromDB = userRepository.findByEmail(user.getName());
-
-        if (!userFromDB.isEmpty()) {
-            return false;
-        }
-
-        user.setRoles(Collections.singleton(new Role(2, "ROLE_USER")));
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return true;
+        return super.saveUser(user);
     }
-    @Transactional
+
+    @Override
     public void deleteUser(int userId) {
-        userRepository.deleteById(userId);
+        super.deleteUser(userId);
     }
 
-
+    @Override
     public Optional<User> findUserById(int userId) {
-        return userRepository.findById(userId);
+        return super.findUserById(userId);
     }
 
-    @Transactional
+    @Override
     public void updateUserByIdAndUser(User user, int userId) {
-        user.setId(userId);
-        userRepository.save(user);
+        super.updateUserByIdAndUser(user, userId);
     }
 
-    public List<String> getRoleInStringById(int id){
-        Optional<User> user = userRepository.findById(id);
-        return user.isEmpty() ? null : user.get().getRoles().stream().map(role -> role.getName()).collect(Collectors.toList());
-
+    @Override
+    public List<String> getRoleInStringById(int id) {
+        return super.getRoleInStringById(id);
     }
 }
